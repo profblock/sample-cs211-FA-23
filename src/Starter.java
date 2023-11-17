ca0770e9e23b171a7273d4f1c58a5c7e4b130a5b
Aaron Block <adblock@gmail.com>
2023-11-17 09:33:19 -0600

    In the main

--- src/Starter.java
+++ src/Starter.java
@@ -2,5 +2,6 @@ public class Starter {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Lets add another line");
+        System.out.println("I'm in the main");
     }
 }
